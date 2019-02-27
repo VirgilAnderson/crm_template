fb051494292868b8fdefc9408b8763790ff68117
Virgil Anderson <Virgil.Anderson@gmail.com>
2019-02-27 14:55:14 -0700

    Feat: Added icons to leads/detail.php

--- public/leads/detail.php
+++ public/leads/detail.php
@@ -61,19 +61,19 @@
                 <div class="card-header">
                 <ul class="nav nav-tabs" role="tablist">
                   <li class="nav-item">
-                    <a class="nav-link active" data-toggle="tab" href="#company_pane">Company</a>
+                    <a class="nav-link active" data-toggle="tab" href="#company_pane"><i class="far fa-building"></i> Company</a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" data-toggle="tab" href="#project_pane">Project</a>
+                    <a class="nav-link" data-toggle="tab" href="#project_pane"><i class="fas fa-project-diagram"></i> Projects</a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" data-toggle="tab" href="#task_pane">Tasks</a>
+                    <a class="nav-link" data-toggle="tab" href="#task_pane"><i class="fas fa-thumbtack"></i> Tasks</a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" data-toggle="tab" href="#history_pane">History</a>
+                    <a class="nav-link" data-toggle="tab" href="#history_pane"><i class="fas fa-history"></i> History</a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" data-toggle="tab" href="#note_pane">Notes</a>
+                    <a class="nav-link" data-toggle="tab" href="#note_pane"><i class="far fa-sticky-note"></i> Notes</a>
                   </li>
                 </ul><!-- .nav nav-tabs -->
 
