45864535f082bbf193b487bea886c495a4858625
Virgil Anderson <Virgil.Anderson@gmail.com>
2019-02-28 12:02:48 -0700

    Feat: Added content to account/edit.php

--- public/account/edit.php
+++ public/account/edit.php
@@ -1,23 +1,22 @@
 <?php require_once('../../private/initialize.php'); ?>
-<?php $page_title = "Edit User"; ?>
+<?php $page_title = "Edit Account"; ?>
 <?php include('../../private/shared/header.php'); ?>
 
 <!-- breadcrumbs -->
 <div class="container" style="margin-top:20px">
   <ol class="breadcrumb">
     <li class="breadcrumb-item"><a href="<?php echo url_for('index.php'); ?>"><i class="fas fa-tachometer-alt"></i> Dashboard</a></li>
-    <li class="breadcrumb-item"><a href='<?php echo url_for('users/index.php'); ?>'><i class="fas fa-users"></i> Users</a></li>
-    <li class="breadcrumb-item"><a href=""><i class="fas fa-info-circle"></i> User Details</a></li>
-    <li class="breadcrumb-item"><i class="far fa-edit"></i> Edit User</li>
+    <li class="breadcrumb-item"><a href='<?php echo url_for('account/index.php'); ?>'><i class="fas fa-user-circle"></i> Account</a></li>
+    <li class="breadcrumb-item"><i class="far fa-edit"></i> Edit Account</li>
   </ol>
 
   <form class="col-sm-6" action="" method="post">
-      <h2><i class="far fa-edit"></i> Edit User</h2>
+      <h2><i class="far fa-edit"></i> Edit Account</h2>
       <fieldset class="form-group">
-        <legend>Lead Information</legend>
+        <legend>Account Information</legend>
 
         <?php include('sections/form_fields.php'); ?>
-        <button class="btn btn-outline-info" type="submit"><i class="far fa-edit"></i> Edit User</button>
+        <button class="btn btn-outline-info" type="submit"><i class="far fa-edit"></i> Edit Account</button>
 
       </fieldset><!-- fieldset -->
     </form>
