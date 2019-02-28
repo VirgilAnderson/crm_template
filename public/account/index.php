dcd33175c41930bf352576951e6fba586215ef54
Virgil Anderson <Virgil.Anderson@gmail.com>
2019-02-28 11:57:55 -0700

    Feat: Updated content for account/index.php

--- public/account/index.php
+++ public/account/index.php
@@ -1,12 +1,12 @@
 <?php require_once('../../private/initialize.php'); ?>
-<?php $page_title = "Users"; ?>
+<?php $page_title = "Account"; ?>
 <?php include('../../private/shared/header.php'); ?>
 
 <!-- breadcrumbs -->
 <div class="container" style="margin-top:20px">
   <ol class="breadcrumb">
     <li class="breadcrumb-item"><a href="<?php echo url_for('index.php'); ?>"><i class="fas fa-tachometer-alt"></i> Dashboard</a></li>
-    <li class="breadcrumb-item active"><i class="fas fa-users"></i> Users</li>
+    <li class="breadcrumb-item active"><i class="fas fa-user-circle"></i> Account</li>
   </ol>
 </div><!-- .container mt-4 -->
 
@@ -15,51 +15,37 @@
     <div class="container col-12 mb-4">
       <div class="card">
         <div class="card-header">
-          <h2><i class="fas fa-users"></i> Users</h2>
+          <h2><i class="fas fa-user-circle"></i> Account</h2>
         </div><!-- .card-header -->
         <div class="card-body">
-          <div class="table-responsive">
-            <table class="table table-hover table-sm">
-              <thead>
-                <tr>
-                  <th>First Name</th>
-                  <th>Last Name</th>
-                  <th>Email</th>
-                  <th>Username</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr class='clickable-row' data-href="">
-                  <td>first_name</td>
-                  <td>last_name</td>
-                  <td>email</td>
-                  <td>username</td>
-                </tr>
-            </tbody>
-            </table>
-          </div><!-- .table-responsive -->
-
-          <!-- pagination -->
-          <ul class="pagination pagination-sm justify-content-center">
-            <li class="page-item">
-              <a class="page-link" href="#">Previous</a>
-            </li>
-            <li class="page-item">
-              <a class="page-link" href="#">1</a>
-            </li>
-            <li class="page-item active">
-              <a class="page-link" href="#">2</a>
-            </li>
-            <li class="page-item">
-              <a class="page-link" href="#">3</a>
-            </li>
-            <li class="page-item">
-              <a class="page-link" href="#">Next</a>
-            </li>
-          </ul><!-- .pagination -->
+          <div class="card-body">
+            <ul class="list-group">
+            <dl class="list-group-item d-flex">
+              <dt class="mr-4">First Name</dt>
+              <dd>First_name</dd>
+            </dl>
+            <dl class="list-group-item d-flex">
+              <dt class="mr-4">Last Name</dt>
+              <dd>Last_name</dd>
+            </dl>
+            <dl class="list-group-item d-flex">
+              <dt class="mr-4">Role</dt>
+              <dd>Role</dd>
+            </dl>
+            <dl class="list-group-item d-flex">
+              <dt class="mr-4">Email</dt>
+              <dd>Email_address</dd>
+            </dl>
+            <dl class="list-group-item d-flex">
+              <dt class="mr-4">Username</dt>
+              <dd>Username</dd>
+            </dl>
         </div><!-- .card-body -->
         <div class="card-footer">
-          <a href="<?php echo url_for('leads/new.php'); ?>" class="btn btn-outline-info mb-2" role="button"><i class="far fa-plus-square"></i> New User</a>
+          <dl class="list-group-item d-flex">
+            <dt class="mr-4"><a class="card-link mr-4" href=""><i class="far fa-trash-alt"></i> Delete Account</a></dt>
+            <dt><a class="card-link" href=""><i class="far fa-edit"></i> Edit Account</a></dt>
+          </dl>
         </div><!-- .card-footer -->
       </div><!-- .card -->
     </div><!-- .container col-sm-12 -->
