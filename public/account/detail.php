bc0932460765275be0b39300cea046b31f791c56
Virgil Anderson <Virgil.Anderson@gmail.com>
2019-02-28 11:20:37 -0700

    Feat: Added account directory

---
+++ public/account/detail.php
@@ -0,0 +1,70 @@
+<?php require_once('../../private/initialize.php'); ?>
+<?php $page_title = "Lead Details"; ?>
+<?php include('../../private/shared/header.php'); ?>
+
+<!-- breadcrumbs -->
+<div class="container" style="margin-top:20px">
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item"><a href="<?php echo url_for('index.php'); ?>"><i class="fas fa-tachometer-alt"></i> Dashboard</a></li>
+    <li class="breadcrumb-item"><a href="<?php echo url_for('leads/index.php'); ?>"><i class="fas fa-dollar-sign"></i> Leads</a></li>
+    <li class="breadcrumb-item active"><i class="fas fa-info-circle"></i> Lead Details</li>
+  </ol>
+</div><!-- .container mt-4 -->
+
+<!-- main container -->
+<div class='container'>
+  <div class="row">
+
+    <!-- Lead Card -->
+    <div class="container col-12 mb-4">
+      <div class="card">
+
+        <!-- Card Header -->
+        <div class="card-header">
+          <h2><i class="fas fa-info-circle"></i> Lead Details</h2>
+        </div><!-- .card-header -->
+        <div class="card-body">
+          <div class="row">
+            <div class="col-sm-5">
+              <!-- Main section Leads -->
+              <?php include('sections/lead_group.php'); ?>
+            </div><!-- .col-sm-5  -->
+
+            <!-- Group Pane Section -->
+            <div class="col-sm-7">
+              <div class="card bg-light">
+                <div class="card-header">
+
+                  <!-- Group Pane Tabs -->
+                  <?php include('sections/group_tabs.php'); ?>
+                </div><!-- .card-header -->
+
+                <!-- Group pane content -->
+                <div class="card-body">
+                  <div class="tab-content">
+                    <?php include('sections/company_pane.php'); ?>
+                    <?php include('sections/project_pane.php'); ?>
+                    <?php include('sections/task_pane.php'); ?>
+                    <?php include('sections/history_pane.php'); ?>
+                    <?php include('sections/note_pane.php'); ?>
+                  </div><!-- .tab-content -->
+                </div><!-- .card-body -->
+              </div><!-- .card -->
+            </div><!-- .col-7 -->
+          </div><!-- .row -->
+        </div><!-- .card-body -->
+        <div class="card-footer">
+          <dl class="list-group-item d-flex">
+            <dt class="mr-4">
+              <a class="card-link mr-4" href=""><i class="far fa-trash-alt"></i> Delete Lead</a>
+            </dt>
+            <dt>
+              <a class="card-link" href=""><i class="far fa-edit"></i> Edit Lead</a>
+            </dt>
+          </dl>
+        </div><!-- .card-footer -->
+      </div><!-- .card -->
+    </div><!-- .container col-sm-12 -->
+  </div><!-- . row -->
+</div><!-- .container -->
+<?php include('../../private/shared/footer.php'); ?>
