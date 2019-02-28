abdafe2e9d643ea26c5185e74c26ff8dfe663f64
Virgil Anderson <Virgil.Anderson@gmail.com>
2019-02-28 12:04:56 -0700

    Feat: Added account/delete.php

--- public/account/delete.php
+++ public/account/delete.php
@@ -1,15 +1,15 @@
 <?php require_once('../../private/initialize.php'); ?>
-<?php $page_title = "Delete User"; ?>
+<?php $page_title = "Delete Account"; ?>
 <?php include('../../private/shared/header.php'); ?>
 
 <!-- breadcrumbs -->
 <div class="container" style="margin-top:20px">
   <ol class="breadcrumb">
     <li class="breadcrumb-item"><a href="<?php echo url_for('index.php'); ?>"><i class="fas fa-tachometer-alt"></i> Dashboard</a></li>
-    <li class="breadcrumb-item"><a href='<?php echo url_for('users/index.php'); ?>'><i class="fas fa-users"></i> Users</a></li>
-    <li class="breadcrumb-item"><a href=""><i class="fas fa-info-circle"></i> User Details</a></li>
-    <li class="breadcrumb-item"><i class="far fa-trash-alt"></i> Delete User</li>
+    <li class="breadcrumb-item"><a href='<?php echo url_for('account/index.php'); ?>'><i class="fas fa-user-circle"></i> Account</a></li>
+    <li class="breadcrumb-item"><i class="far fa-trash-alt"></i> Delete Account</li>
   </ol>
+
 </div><!-- .container mt-4 -->
 
 <!-- main container -->
@@ -18,11 +18,11 @@
     <div class="container col-12 mb-4">
       <div class="card">
         <div class="card-header">
-          <h2><i class="far fa-trash-alt"></i> Delete User</h2>
+          <h2><i class="far fa-trash-alt"></i> Delete Account</h2>
         </div><!-- .card-header -->
         <div class="card-body">
           <p>Are you sure you want to delete?</p>
-          <p>User Name</p>
+          <p>Account Name</p>
           <form class="col-sm-6" action="" method="post">
             <fieldset class="form-group">
               <button class="btn btn-outline-info" type="submit">Delete</button>
@@ -31,11 +31,8 @@
         </div><!-- .card-body -->
         <div class="card-footer">
           <dl class="list-group-item d-flex">
-            <dt class="mr-4">
-              <a class="card-link mr-4" href=""><i class="fas fa-info-circle"></i> User Detail</a>
-            </dt>
             <dt>
-              <a class="card-link" href=""><i class="far fa-edit"></i> Edit User</a>
+              <a class="card-link" href=""><i class="far fa-edit"></i> Edit Account</a>
             </dt>
           </dl>
         </div><!-- .card-footer -->
