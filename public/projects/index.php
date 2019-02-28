e33b50ac37a494ebb764097bad0ee8d8026db4b9
Virgil Anderson <Virgil.Anderson@gmail.com>
2019-02-27 21:35:50 -0700

    Feat: Updated template

--- public/projects/index.php
+++ public/projects/index.php
@@ -1,12 +1,12 @@
 <?php require_once('../../private/initialize.php'); ?>
-<?php $page_title = "Leads"; ?>
+<?php $page_title = "Projects"; ?>
 <?php include('../../private/shared/header.php'); ?>
 
 <!-- breadcrumbs -->
 <div class="container" style="margin-top:20px">
   <ol class="breadcrumb">
     <li class="breadcrumb-item"><a href="<?php echo url_for('index.php'); ?>"><i class="fas fa-tachometer-alt"></i> Dashboard</a></li>
-    <li class="breadcrumb-item active"><i class="fas fa-dollar-sign"></i> Leads</li>
+    <li class="breadcrumb-item active"><i class="fas fa-project-diagram"></i> Projects</li>
   </ol>
 </div><!-- .container mt-4 -->
 
@@ -15,34 +15,25 @@
     <div class="container col-12 mb-4">
       <div class="card">
         <div class="card-header">
-          <h2><i class="fas fa-dollar-sign"></i> Leads</h2>
+          <h2><i class="fas fa-project-diagram"></i> Projects</h2>
         </div><!-- .card-header -->
         <div class="card-body">
           <div class="table-responsive">
             <table class="table table-hover table-sm">
               <thead>
                 <tr>
-                  <th></th>
-                  <th>First Name</th>
-                  <th>Last Name</th>
-                  <th>Phone</th>
-                  <th>Email</th>
-                  <th>Role</th>
-                  <th>Lead Source</th>
-                  <th>Date Added</th>
+                  <th>Project Title</th>
+                  <th>Project State</th>
+                  <th>Company</th>
+                  <th>Project Owner</th>
                 </tr>
               </thead>
               <tbody>
-
                 <tr class='clickable-row' data-href="">
-                  <td><span class='badge badge-info'>new</span></td>
-                  <td>first_name</td>
-                  <td>last_name</td>
-                  <td>phone_direct</td>
-                  <td>email</td>
-                  <td>role</td>
-                  <td>lead_source</td>
-                  <td>lead_birthdate</td>
+                  <td>project_title</td>
+                  <td>project_state</td>
+                  <td>company_name</td>
+                  <td>username</td>
                 </tr>
             </tbody>
             </table>
@@ -68,7 +59,7 @@
           </ul><!-- .pagination -->
         </div><!-- .card-body -->
         <div class="card-footer">
-          <a href="<?php echo url_for('leads/new.php'); ?>" class="btn btn-outline-info mb-2" role="button"><i class="far fa-plus-square"></i> new lead</a>
+          <a href="<?php echo url_for('projects/new.php'); ?>" class="btn btn-outline-info mb-2" role="button"><i class="far fa-plus-square"></i> New Project</a>
         </div><!-- .card-footer -->
       </div><!-- .card -->
     </div><!-- .container col-sm-12 -->
