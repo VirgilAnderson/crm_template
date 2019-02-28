77aa6e2ef19f414634d4f5add97bc76f133e7985
Virgil Anderson <Virgil.Anderson@gmail.com>
2019-02-27 21:08:55 -0700

    Feat: Added template content to companies/new.php

--- public/companies/new.php
+++ public/companies/new.php
@@ -6,21 +6,21 @@
 <div class="container" style="margin-top:20px">
   <ol class="breadcrumb">
     <li class="breadcrumb-item"><a href="<?php echo url_for('index.php'); ?>"><i class="fas fa-tachometer-alt"></i> Dashboard</a></li>
-    <li class="breadcrumb-item"><a href="<?php echo url_for('leads/index.php'); ?>"><i class="fas fa-dollar-sign"></i> Leads</a></li>
-    <li class="breadcrumb-item active"><i class="far fa-plus-square"></i> New Lead</li>
+    <li class="breadcrumb-item active"><a href="<?php echo url_for('companies/index.php'); ?>"><i class="far fa-building"></i> Companies</a></li>
+    <li class="breadcrumb-item active"><i class="far fa-plus-square"></i> New Company</li>
   </ol>
 </div><!-- .container mt-4 -->
 
 <!-- main container -->
 <div class='container'>
   <form class="col-sm-6" action=""  method="post">
-      <h2><i class="far fa-plus-square"></i> New Lead</h2>
+      <h2><i class="far fa-plus-square"></i> New Company</h2>
       <fieldset class="form-group">
-        <legend>Fill in the form to create a new record</legend>
+        <legend>Fill in the form to create a new company</legend>
 
         <?php include('sections/form_fields.php'); ?>
 
-        <button class="btn btn-outline-info" type="submit">Create New Lead</button>
+        <button class="btn btn-outline-info" type="submit">Create New Company</button>
 
       </fieldset><!-- fieldset -->
     </form>
--- public/companies/sections/form_fields.php
+++ public/companies/sections/form_fields.php
@@ -1,61 +1,43 @@
+<div class="form-group">
+  <label class="form-control-label" for="company_name">Company Name</label>
+  <input class="form-control" type="text" name="company_name" >
+</div><!-- form-group -->
 
+<div class="form-group">
+  <label class="form-control-label" for="company_address">Company Address</label>
+  <input class="form-control" type="text" name="company_address" >
+</div><!-- form-group -->
 
-      <div class="form-group">
-        <label class="form-control-label" for="first_name">First Name</label>
-        <input class="form-control" type="text" name="first_name" >
-      </div><!-- form-group -->
+<div class="form-group">
+  <label class="form-control-label" for="company_city">Company City</label>
+  <input class="form-control" type="text" name="company_city" >
+</div><!-- form-group -->
 
-      <div class="form-group">
-        <label class="form-control-label" for="last_name">Last Name</label>
-        <input class="form-control" type="text" name="last_name" >
-      </div><!-- form-group -->
+<div class="form-group">
+  <label class="form-control-label" for="company_state">Company State</label>
+  <input class="form-control" type="text" name="company_state">
+</div><!-- form-group -->
 
-      <div class="form-group">
-        <label class="form-control-label" for="phone_direct">Phone</label>
-        <input class="form-control" type="text" name="phone_direct" >
-      </div><!-- form-group -->
+<div class="form-group">
+  <label class="form-control-label" for="company_zip">Company Zip</label>
+  <input class="form-control" type="text" name="company_zip">
+</div><!-- form-group -->
 
-      <div class="form-group">
-        <label class="form-control-label" for="email">Email</label>
-        <input class="form-control" type="text" name="email">
-      </div><!-- form-group -->
+<div class="form-group">
+  <label class="form-control-label" for="company_url">Company url</label>
+  <input class="form-control" type="text" name="company_url">
+</div><!-- form-group -->
 
-      <div class="form-group">
-        <label class="form-control-label" for="role">Role</label>
-        <input class="form-control" type="text" name="role">
-      </div><!-- form-group -->
+<div class="form-group">
+  <label class="form-control-label" for="company_phone">Phone</label>
+  <input class="form-control" type="text" name="company_phone">
+</div><!-- form-group -->
 
-      <div class="form-group">
-        <label for="lead_source">Select list:</label>
-          <select class="form-control" name="lead_source">
-            <option>Web</option>
-            <option>Manual Entry</option>
-            <option>Lead List</option>
-            <option>Call In</option>
-          </select>
-      </div><!-- form-group -->
-
-      <div class="form-group">
-        <label for="company_id">Company:</label>
-            <select class="form-control" name="company_id">
-              <option value='none'>none</option>
-              <option value="">Company</option>
-            </select>
-      </div><!-- form-group -->
-
-      <div class="form-group">
-        <label for="project_id">Project:</label>
-            <select class="form-control" name="project_id">
-              <option value='none'>none</option>
-              <option value="">Project Title</option>
-            </select>
-      </div><!-- form-group -->
-
-      <div class="form-group">
-        <label for="lead_source">Lead Owner:</label>
-          <select class="form-control" name="user_id">
-            <option value="" ></option>
-          </select>
-      </div><!-- form-group -->
-
-      
+<div class="form-group">
+  <label for="lead_source">Lead Owner:</label>
+    <select class="form-control" name="user_id">
+      <?php while($admin = mysqli_fetch_assoc($admin_set)){ ?>
+      <option value="<?php echo h($admin['id']); ?>"><?php echo h($admin['username']); ?></option>
+      <?php } ?>
+    </select>
+</div><!-- form-group -->
