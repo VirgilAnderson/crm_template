940f4c2846bd77ec6314646fcc59a25516a379ee
Virgil Anderson <Virgil.Anderson@gmail.com>
2019-02-27 21:49:33 -0700

    Feat: Added template content

--- public/projects/edit.php
+++ public/projects/edit.php
@@ -1,23 +1,23 @@
 <?php require_once('../../private/initialize.php'); ?>
-<?php $page_title = "Edit lead"; ?>
+<?php $page_title = "Edit Project"; ?>
 <?php include('../../private/shared/header.php'); ?>
 
 <!-- breadcrumbs -->
 <div class="container" style="margin-top:20px">
   <ol class="breadcrumb">
     <li class="breadcrumb-item"><a href="<?php echo url_for('index.php'); ?>"><i class="fas fa-tachometer-alt"></i> Dashboard</a></li>
-    <li class="breadcrumb-item"><a href="<?php echo url_for('leads/index.php'); ?>"><i class="fas fa-dollar-sign"></i> Leads</a></li>
-    <li class="breadcrumb-item"><a href=""><i class="fas fa-info-circle"></i> Lead Detail</a></li>
-    <li class="breadcrumb-item active"><i class="far fa-edit"></i> Edit Lead</li>
+    <li class="breadcrumb-item"><a href='<?php echo url_for('projects/index.php'); ?>'><i class="fas fa-project-diagram"></i> Projects</a></li>
+    <li class="breadcrumb-item"><a href=''><i class="fas fa-info-circle"></i> Project Details</a></li>
+    <li class="breadcrumb-item active"><i class="far fa-plus-square"></i> Edit Project</li>
   </ol>
 
   <form class="col-sm-6" action="" method="post">
-      <h2><i class="far fa-edit"></i> Edit Lead</h2>
+      <h2><i class="far fa-edit"></i> Edit Project</h2>
       <fieldset class="form-group">
-        <legend>Lead Information</legend>
+        <legend>Project Information</legend>
 
         <?php include('sections/form_fields.php'); ?>
-        <button class="btn btn-outline-info" type="submit"><i class="far fa-edit"></i> Edit Lead</button>
+        <button class="btn btn-outline-info" type="submit"><i class="far fa-edit"></i> Project Lead</button>
 
       </fieldset><!-- fieldset -->
     </form>
