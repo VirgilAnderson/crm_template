d1b1c631a11ee622f7b1c69aa79afa200f013369
Virgil Anderson <Virgil.Anderson@gmail.com>
2019-02-27 20:38:51 -0700

    Feat: Added content to task/edit.php

--- public/tasks/edit.php
+++ public/tasks/edit.php
@@ -1,23 +1,23 @@
 <?php require_once('../../private/initialize.php'); ?>
-<?php $page_title = "Edit lead"; ?>
+<?php $page_title = "Edit Task"; ?>
 <?php include('../../private/shared/header.php'); ?>
 
 <!-- breadcrumbs -->
 <div class="container" style="margin-top:20px">
   <ol class="breadcrumb">
     <li class="breadcrumb-item"><a href="<?php echo url_for('index.php'); ?>"><i class="fas fa-tachometer-alt"></i> Dashboard</a></li>
-    <li class="breadcrumb-item"><a href="<?php echo url_for('leads/index.php'); ?>"><i class="fas fa-dollar-sign"></i> Leads</a></li>
-    <li class="breadcrumb-item"><a href=""><i class="fas fa-info-circle"></i> Lead Detail</a></li>
-    <li class="breadcrumb-item active"><i class="far fa-edit"></i> Edit Lead</li>
+    <li class="breadcrumb-item"><a href="<?php echo url_for('tasks/index.php'); ?>"><i class="fas fa-thumbtack"></i> Tasks</a></li>
+    <li class="breadcrumb-item"><a href=""><i class="fas fa-info-circle"></i> Task Detail</a></li>
+    <li class="breadcrumb-item active"><i class="far fa-edit"></i> Edit Task</li>
   </ol>
 
   <form class="col-sm-6" action="" method="post">
-      <h2><i class="far fa-edit"></i> Edit Lead</h2>
+      <h2><i class="far fa-edit"></i> Edit Task</h2>
       <fieldset class="form-group">
-        <legend>Lead Information</legend>
+        <legend>Task Information</legend>
 
         <?php include('sections/form_fields.php'); ?>
-        <button class="btn btn-outline-info" type="submit"><i class="far fa-edit"></i> Edit Lead</button>
+        <button class="btn btn-outline-info" type="submit"><i class="far fa-edit"></i> Edit Task</button>
 
       </fieldset><!-- fieldset -->
     </form>
