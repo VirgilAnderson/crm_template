e56aa5ba2180dada88c4c9632828fab9060909f0
Virgil Anderson <Virgil.Anderson@gmail.com>
2019-02-27 19:39:11 -0700

    Feat: Moved group panes to their own page on leads/detail.php

---
+++ public/leads/sections/lead_group.php
@@ -0,0 +1,30 @@
+<ul class="list-group list-group-flush">
+  <dl class="list-group-item d-flex">
+    <dt class="mr-4">Name</dt>
+    <dd>First Name Last Name</dd>
+  </dl>
+  <dl class="list-group-item d-flex">
+    <dt class="mr-4">Phone</dt>
+    <dd>Phone</dd>
+  </dl>
+  <dl class="list-group-item d-flex">
+    <dt class="mr-4">Email</dt>
+    <dd>Email</dd>
+  </dl>
+  <dl class="list-group-item d-flex">
+    <dt class="mr-4">Role</dt>
+    <dd>Role</dd>
+  </dl>
+  <dl class="list-group-item d-flex">
+    <dt class="mr-4">Lead source</dt>
+    <dd>Lead Source</dd>
+  </dl>
+  <dl class="list-group-item d-flex">
+    <dt class="mr-4">Date Created</dt>
+    <dd>Lead Birthdate</dd>
+  </dl>
+  <dl class="list-group-item d-flex">
+    <dt class="mr-4">Lead Owner</dt>
+    <dd>Lead Owner</dd>
+  </dl>
+</ul>
