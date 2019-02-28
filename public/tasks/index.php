e2a51e17d27a3809c61d3352afa104e6bdb2aed1
Virgil Anderson <Virgil.Anderson@gmail.com>
2019-02-27 20:24:26 -0700

    Feat: Updated task/index.php template

--- public/tasks/index.php
+++ public/tasks/index.php
@@ -6,7 +6,7 @@
 <div class="container" style="margin-top:20px">
   <ol class="breadcrumb">
     <li class="breadcrumb-item"><a href="<?php echo url_for('index.php'); ?>"><i class="fas fa-tachometer-alt"></i> Dashboard</a></li>
-    <li class="breadcrumb-item active"><i class="fas fa-dollar-sign"></i> Leads</li>
+    <li class="breadcrumb-item active"><i class="fas fa-thumbtack"></i> Tasks</li>
   </ol>
 </div><!-- .container mt-4 -->
 
@@ -15,34 +15,30 @@
     <div class="container col-12 mb-4">
       <div class="card">
         <div class="card-header">
-          <h2><i class="fas fa-dollar-sign"></i> Leads</h2>
+          <h2><i class="fas fa-thumbtack"></i> Tasks</h2>
         </div><!-- .card-header -->
         <div class="card-body">
           <div class="table-responsive">
             <table class="table table-hover table-sm">
               <thead>
                 <tr>
-                  <th></th>
-                  <th>First Name</th>
-                  <th>Last Name</th>
-                  <th>Phone</th>
-                  <th>Email</th>
-                  <th>Role</th>
-                  <th>Lead Source</th>
-                  <th>Date Added</th>
+                  <th>Title</th>
+                  <th>Task Type</th>
+                  <th>Task State</th>
+                  <th>Employee</th>
+                  <th>Company</th>
+                  <th>Due Date</th>
+
                 </tr>
               </thead>
               <tbody>
-
                 <tr class='clickable-row' data-href="">
-                  <td><span class='badge badge-info'>new</span></td>
+                  <td>task_name</td>
+                  <td>task_type</td>
+                  <td>task_state</td>
                   <td>first_name</td>
-                  <td>last_name</td>
-                  <td>phone_direct</td>
-                  <td>email</td>
-                  <td>role</td>
-                  <td>lead_source</td>
-                  <td>lead_birthdate</td>
+                  <td>company_name</td>
+                  <td>due_date</td>
                 </tr>
             </tbody>
             </table>
@@ -68,7 +64,7 @@
           </ul><!-- .pagination -->
         </div><!-- .card-body -->
         <div class="card-footer">
-          <a href="<?php echo url_for('leads/new.php'); ?>" class="btn btn-outline-info mb-2" role="button"><i class="far fa-plus-square"></i> new lead</a>
+          <a href="<?php echo url_for('leads/new.php'); ?>" class="btn btn-outline-info mb-2" role="button"><i class="far fa-plus-square"></i> new task</a>
         </div><!-- .card-footer -->
       </div><!-- .card -->
     </div><!-- .container col-sm-12 -->
