309c82bcf679fa2bc5842eab3f4cbc600c2f2134
Virgil Anderson <Virgil.Anderson@gmail.com>
2019-02-28 13:04:37 -0700

    Refactor: Updated fields to match database

--- public/companies/detail_section/company_detail.php
+++ public/companies/detail_section/company_detail.php
@@ -8,23 +8,27 @@
     <dd>company_city</dd>
   </dl>
   <dl class="list-group-item d-flex">
+    <dt class="mr-4">State</dt>
+    <dd>company_state</dd>
+  </dl>
+  <dl class="list-group-item d-flex">
     <dt class="mr-4">Zip</dt>
     <dd>company_zip</dd>
   </dl>
   <dl class="list-group-item d-flex">
-    <dt class="mr-4">State</dt>
-    <dd>company_state</dd>
+    <dt class="mr-4">Company Phone</dt>
+    <dd>Company Phone</dd>
   </dl>
   <dl class="list-group-item d-flex">
     <dt class="mr-4">URL</dt>
     <dd>company_url</dd>
   </dl>
   <dl class="list-group-item d-flex">
-    <dt class="mr-4">Phone</dt>
-    <dd>company_phone</dd>
+    <dt class="mr-4">Industry</dt>
+    <dd>Industry</dd>
   </dl>
   <dl class="list-group-item d-flex">
-    <dt class="mr-4">Lead Owner</dt>
+    <dt class="mr-4">Company Owner</dt>
     <dd>username</dd>
   </dl>
 </ul>
