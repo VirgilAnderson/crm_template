cfbe640973d455e74641c4593f2e4b030c978169
Virgil Anderson <Virgil.Anderson@gmail.com>
2019-02-28 23:52:01 -0700

    Style: updated style on company_detail.php to make it more readible

--- public/companies/detail_section/company_detail.php
+++ public/companies/detail_section/company_detail.php
@@ -1,34 +1,34 @@
 <ul class="list-group list-group-flush">
-  <dl class="list-group-item d-flex">
-    <dt class="mr-4">Address</dt>
+  <dl class="list-group-item">
+    <dt>Address</dt>
     <dd>company_address</dd>
   </dl>
-  <dl class="list-group-item d-flex">
-    <dt class="mr-4">City</dt>
+  <dl class="list-group-item">
+    <dt>City</dt>
     <dd>company_city</dd>
   </dl>
-  <dl class="list-group-item d-flex">
-    <dt class="mr-4">State</dt>
+  <dl class="list-group-item">
+    <dt>State</dt>
     <dd>company_state</dd>
   </dl>
-  <dl class="list-group-item d-flex">
-    <dt class="mr-4">Zip</dt>
+  <dl class="list-group-item">
+    <dt>Zip</dt>
     <dd>company_zip</dd>
   </dl>
-  <dl class="list-group-item d-flex">
-    <dt class="mr-4">Company Phone</dt>
+  <dl class="list-group-item">
+    <dt>Company Phone</dt>
     <dd>Company Phone</dd>
   </dl>
-  <dl class="list-group-item d-flex">
-    <dt class="mr-4">URL</dt>
+  <dl class="list-group-item">
+    <dt>URL</dt>
     <dd>company_url</dd>
   </dl>
-  <dl class="list-group-item d-flex">
-    <dt class="mr-4">Industry</dt>
+  <dl class="list-group-item">
+    <dt>Industry</dt>
     <dd>Industry</dd>
   </dl>
-  <dl class="list-group-item d-flex">
-    <dt class="mr-4">Company Owner</dt>
+  <dl class="list-group-item">
+    <dt>Company Owner</dt>
     <dd>username</dd>
   </dl>
 </ul>
