b4ac61f8d39f6146565d2ea4404aba87368f53f4
Virgil Anderson <Virgil.Anderson@gmail.com>
2019-03-01 00:05:04 -0700

    Style: Updated styles to make more readable

--- public/leads/detail_section/lead_detail.php
+++ public/leads/detail_section/lead_detail.php
@@ -1,38 +1,38 @@
 <ul class="list-group list-group-flush">
-  <dl class="list-group-item d-flex">
-    <dt class="mr-4">First Name</dt>
+  <dl class="list-group-item">
+    <dt>First Name</dt>
     <dd>First Name</dd>
   </dl>
-  <dl class="list-group-item d-flex">
-    <dt class="mr-4">Last Name</dt>
+  <dl class="list-group-item">
+    <dt>Last Name</dt>
     <dd>Last Name</dd>
   </dl>
-  <dl class="list-group-item d-flex">
-    <dt class="mr-4">Phone</dt>
+  <dl class="list-group-item">
+    <dt>Phone</dt>
     <dd>Phone</dd>
   </dl>
-  <dl class="list-group-item d-flex">
-    <dt class="mr-4">Mobile</dt>
+  <dl class="list-group-item">
+    <dt>Mobile</dt>
     <dd>Mobile</dd>
   </dl>
-  <dl class="list-group-item d-flex">
-    <dt class="mr-4">Email</dt>
+  <dl class="list-group-item">
+    <dt>Email</dt>
     <dd>Email</dd>
   </dl>
-  <dl class="list-group-item d-flex">
-    <dt class="mr-4">Title</dt>
+  <dl class="list-group-item">
+    <dt>Title</dt>
     <dd>Title</dd>
   </dl>
-  <dl class="list-group-item d-flex">
-    <dt class="mr-4">Lead source</dt>
+  <dl class="list-group-item">
+    <dt>Lead source</dt>
     <dd>Lead Source</dd>
   </dl>
-  <dl class="list-group-item d-flex">
-    <dt class="mr-4">Date Created</dt>
+  <dl class="list-group-item">
+    <dt>Date Created</dt>
     <dd>Lead Birthdate</dd>
   </dl>
-  <dl class="list-group-item d-flex">
-    <dt class="mr-4">Lead Owner</dt>
+  <dl class="list-group-item">
+    <dt>Lead Owner</dt>
     <dd>Lead Owner</dd>
   </dl>
 </ul>
--- public/projects/detail_section/project_detail.php
+++ public/projects/detail_section/project_detail.php
@@ -1,26 +1,26 @@
 <ul class="list-group list-group-flush">
-  <dl class="list-group-item d-flex">
-    <dt class="mr-4">Project Title</dt>
+  <dl class="list-group-item">
+    <dt>Project Title</dt>
     <dd>project_title</dd>
   </dl>
 
-  <dl class="list-group-item d-flex">
-    <dt class="mr-4">Project State</dt>
+  <dl class="list-group-item">
+    <dt>Project State</dt>
     <dd>project_state</dd>
   </dl>
 
-  <dl class="list-group-item d-flex">
-    <dt class="mr-4">Project Description</dt>
+  <dl class="list-group-item">
+    <dt>Project Description</dt>
     <dd>project_description</dd>
   </dl>
 
-  <dl class="list-group-item d-flex">
-    <dt class="mr-4">Company</dt>
+  <dl class="list-group-item">
+    <dt>Company</dt>
     <dd>company_name</dd>
   </dl>
 
-  <dl class="list-group-item d-flex">
-    <dt class="mr-4">Project Owner</dt>
+  <dl class="list-group-item">
+    <dt>Project Owner</dt>
     <dd>username</dd>
   </dl>
 </ul>
