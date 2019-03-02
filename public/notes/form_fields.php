2d049067b26167862a3341667a9bc8db3a594efe
Virgil Anderson <Virgil.Anderson@gmail.com>
2019-03-02 12:49:10 -0700

    Feat: Added form content to form_fields.php

--- public/notes/form_fields.php
+++ public/notes/form_fields.php
@@ -1,43 +1,13 @@
 
 
       <div class="form-group">
-        <label class="form-control-label" for="first_name">First Name</label>
-        <input class="form-control" type="text" name="first_name" >
+        <label class="form-control-label" for="">Note Name</label>
+        <input class="form-control" type="text" name="" >
       </div><!-- form-group -->
 
       <div class="form-group">
-        <label class="form-control-label" for="last_name">Last Name</label>
-        <input class="form-control" type="text" name="last_name" >
-      </div><!-- form-group -->
-
-      <div class="form-group">
-        <label class="form-control-label" for="phone_direct">Phone</label>
-        <input class="form-control" type="text" name="phone_direct" >
-      </div><!-- form-group -->
-
-      <div class="form-group">
-        <label class="form-control-label" for="mobile_phone">Mobile</label>
-        <input class="form-control" type="text" name="mobile_phone" >
-      </div><!-- form-group -->
-
-      <div class="form-group">
-        <label class="form-control-label" for="email">Email</label>
-        <input class="form-control" type="text" name="email">
-      </div><!-- form-group -->
-
-      <div class="form-group">
-        <label class="form-control-label" for="title">Title</label>
-        <input class="form-control" type="text" name="title">
-      </div><!-- form-group -->
-
-      <div class="form-group">
-        <label for="lead_source">Select list:</label>
-          <select class="form-control" name="lead_source">
-            <option>Web</option>
-            <option>Manual Entry</option>
-            <option>Lead List</option>
-            <option>Call In</option>
-          </select>
+        <label for="note">Note Description:</label>
+        <textarea class="form-control" rows="5" name="note"></textarea>
       </div><!-- form-group -->
 
       <div class="form-group">
@@ -49,16 +19,25 @@
       </div><!-- form-group -->
 
       <div class="form-group">
+        <label for="">Lead:</label>
+            <select class="form-control" name="">
+              <option value='none'>none</option>
+              <option value="">Lead</option>
+            </select>
+      </div><!-- form-group -->
+
+      <div class="form-group">
+        <label for="">Contact:</label>
+            <select class="form-control" name="">
+              <option value='none'>none</option>
+              <option value="">Contact</option>
+            </select>
+      </div><!-- form-group -->
+
+      <div class="form-group">
         <label for="project_id">Project:</label>
             <select class="form-control" name="project_id">
               <option value='none'>none</option>
               <option value="">Project Title</option>
             </select>
-      </div><!-- form-group -->
-
-      <div class="form-group">
-        <label for="lead_source">Lead Owner:</label>
-          <select class="form-control" name="user_id">
-            <option value="" ></option>
-          </select>
       </div><!-- form-group -->
