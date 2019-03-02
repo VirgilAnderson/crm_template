6791802ae4e4e3dbc13d4b2e733a5aa1f8a4d159
Virgil Anderson <Virgil.Anderson@gmail.com>
2019-03-02 12:27:11 -0700

    Refactor: edited the information that displays for note section

--- public/notes/detail_section/note_detail.php
+++ public/notes/detail_section/note_detail.php
@@ -1,38 +1,13 @@
 <ul class="list-group list-group-flush">
   <dl class="list-group-item">
-    <dt>First Name</dt>
-    <dd>First Name</dd>
-  </dl>
-  <dl class="list-group-item">
-    <dt>Last Name</dt>
-    <dd>Last Name</dd>
-  </dl>
-  <dl class="list-group-item">
-    <dt>Phone</dt>
-    <dd>Phone</dd>
-  </dl>
-  <dl class="list-group-item">
-    <dt>Mobile</dt>
-    <dd>Mobile</dd>
-  </dl>
-  <dl class="list-group-item">
-    <dt>Email</dt>
-    <dd>Email</dd>
-  </dl>
-  <dl class="list-group-item">
-    <dt>Title</dt>
-    <dd>Title</dd>
-  </dl>
-  <dl class="list-group-item">
-    <dt>Lead source</dt>
-    <dd>Lead Source</dd>
+    <h3>Note Name</h3>
   </dl>
   <dl class="list-group-item">
     <dt>Date Created</dt>
-    <dd>Lead Birthdate</dd>
+    <dd>Timestamp</dd>
   </dl>
   <dl class="list-group-item">
-    <dt>Lead Owner</dt>
-    <dd>Lead Owner</dd>
+    <dt>Note Description</dt>
+    <dd>Note Description</dd>
   </dl>
 </ul>
