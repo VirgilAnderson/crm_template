caa4f7bc5cd7bfc0ee4a9b0f62693227038640f1
Virgil Anderson <Virgil.Anderson@gmail.com>
2019-03-02 12:52:51 -0700

    Feat: Updated content

--- public/notes/edit.php
+++ public/notes/edit.php
@@ -1,23 +1,22 @@
 <?php require_once('../../private/initialize.php'); ?>
-<?php $page_title = "Edit lead"; ?>
+<?php $page_title = "Edit Note"; ?>
 <?php include('../../private/shared/header.php'); ?>
 
 <!-- breadcrumbs -->
 <div class="container" style="margin-top:20px">
   <ol class="breadcrumb">
     <li class="breadcrumb-item"><a href="<?php echo url_for('index.php'); ?>"><i class="fas fa-tachometer-alt"></i> Dashboard</a></li>
-    <li class="breadcrumb-item"><a href="<?php echo url_for('leads/index.php'); ?>"><i class="fas fa-dollar-sign"></i> Leads</a></li>
-    <li class="breadcrumb-item"><a href=""><i class="fas fa-info-circle"></i> Lead Detail</a></li>
-    <li class="breadcrumb-item active"><i class="far fa-edit"></i> Edit Lead</li>
+    <li class="breadcrumb-item"><a href=""><i class="fas fa-info-circle"></i> Note Details</a></li>
+    <li class="breadcrumb-item active"><i class="far fa-edit"></i> Edit Note</li>
   </ol>
 
   <form class="col-sm-6" action="" method="post">
-      <h2><i class="far fa-edit"></i> Edit Lead</h2>
+      <h2><i class="far fa-edit"></i> Edit Note</h2>
       <fieldset class="form-group">
-        <legend>Lead Information</legend>
+        <legend>Note Information</legend>
 
         <?php include('form_fields.php'); ?>
-        <button class="btn btn-outline-info" type="submit"><i class="far fa-edit"></i> Edit Lead</button>
+        <button class="btn btn-outline-info" type="submit"><i class="far fa-edit"></i> Edit Note</button>
 
       </fieldset><!-- fieldset -->
     </form>
