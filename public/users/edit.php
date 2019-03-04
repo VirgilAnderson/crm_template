74e047c5a243b3e70f8464ca4ffdd476b2e03254
Virgil Anderson <Virgil.Anderson@gmail.com>
2019-03-04 12:53:50 -0700

    Feat: activated edit functionality

--- public/users/edit.php
+++ public/users/edit.php
@@ -7,6 +7,25 @@
   if($user_id) {
     // Search for user by id
     $user = User::find_by_id($user_id);
+  } else {
+    // If not set, redirect to users page
+    redirect_to(url_for('users/index.php'));
+  }
+
+  // If post request, process the form
+  if(is_post_request()) {
+    // Edit record using post data
+    $args = $_POST['user'];
+    $user->merge_attributes($args);
+    $result = $user->save();
+
+    if($result === true) {
+      //$session->message('The listing was updated successfully.');
+      redirect_to(url_for('/users/detail.php?user_id=' . $user_id));
+    } else {
+      // show errors
+
+    }
   }
 
 ?>
@@ -19,11 +38,11 @@
   <ol class="breadcrumb">
     <li class="breadcrumb-item"><a href="<?php echo url_for('index.php'); ?>"><i class="fas fa-tachometer-alt"></i> Dashboard</a></li>
     <li class="breadcrumb-item"><a href='<?php echo url_for('users/index.php'); ?>'><i class="fas fa-users"></i> Users</a></li>
-    <li class="breadcrumb-item"><a href=""><i class="fas fa-info-circle"></i> User Details</a></li>
+    <li class="breadcrumb-item"><a href="<?php echo url_for('users/detail.php?user_id=' . $user_id); ?>"><i class="fas fa-info-circle"></i> User Details</a></li>
     <li class="breadcrumb-item"><i class="far fa-edit"></i> Edit User</li>
   </ol>
 
-  <form class="col-sm-6" action="" method="post">
+  <form class="col-sm-6" action="<?php echo url_for('/users/edit.php?user_id=' . $user_id); ?>" method="post">
       <h2><i class="far fa-edit"></i> Edit User</h2>
       <fieldset class="form-group">
         <legend>Lead Information</legend>
