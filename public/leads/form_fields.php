5585281301f7ecb9f3617e75cdf60d51a0d2b575
Virgil Anderson <Virgil.Anderson@gmail.com>
2019-03-06 09:53:39 -0700

    Feat: Added dynamic entry of values from Lead.Class.php to Lead Source field

--- public/leads/form_fields.php
+++ public/leads/form_fields.php
@@ -1,64 +1,55 @@
 
 
       <div class="form-group">
-        <label class="form-control-label" for="first_name">First Name</label>
-        <input class="form-control" type="text" name="first_name" >
+        <label class="form-control-label" for="lead[lead_first_name]">First Name</label>
+        <input class="form-control" type="text" name="lead[lead_first_name]" >
       </div><!-- form-group -->
 
       <div class="form-group">
-        <label class="form-control-label" for="last_name">Last Name</label>
-        <input class="form-control" type="text" name="last_name" >
+        <label class="form-control-label" for="lead[lead_last_name]">Last Name</label>
+        <input class="form-control" type="text" name="lead[lead_last_name]" >
       </div><!-- form-group -->
 
       <div class="form-group">
-        <label class="form-control-label" for="phone_direct">Phone</label>
-        <input class="form-control" type="text" name="phone_direct" >
+        <label class="form-control-label" for="lead[lead_phone_number]">Phone</label>
+        <input class="form-control" type="text" name="lead[lead_phone_number]" >
       </div><!-- form-group -->
 
       <div class="form-group">
-        <label class="form-control-label" for="mobile_phone">Mobile</label>
-        <input class="form-control" type="text" name="mobile_phone" >
+        <label class="form-control-label" for="lead[lead_mobile_phone]">Mobile</label>
+        <input class="form-control" type="text" name="lead[lead_mobile_phone]" >
       </div><!-- form-group -->
 
       <div class="form-group">
-        <label class="form-control-label" for="email">Email</label>
-        <input class="form-control" type="text" name="email">
+        <label class="form-control-label" for="lead[lead_email]">Email</label>
+        <input class="form-control" type="text" name="lead[lead_email]">
       </div><!-- form-group -->
 
       <div class="form-group">
-        <label class="form-control-label" for="title">Title</label>
-        <input class="form-control" type="text" name="title">
+        <label class="form-control-label" for="lead[lead_role]">Title</label>
+        <input class="form-control" type="text" name="lead[lead_role]">
       </div><!-- form-group -->
 
       <div class="form-group">
-        <label for="lead_source">Select list:</label>
-          <select class="form-control" name="lead_source">
-            <option>Web</option>
-            <option>Manual Entry</option>
-            <option>Lead List</option>
-            <option>Call In</option>
+        <label for="lead[lead_source]">Lead Source:</label>
+          <select class="form-control" name="lead[lead_source]">
+            <?php foreach(LEAD::LEAD_SOURCE_OPTIONS as $source) { ?>
+              <option value='<?php echo $source; ?>'><?php echo $source; ?></option>
+            <?php } ?>
           </select>
       </div><!-- form-group -->
 
       <div class="form-group">
-        <label for="company_id">Company:</label>
-            <select class="form-control" name="company_id">
+        <label for="lead[lead_company_name]">Company:</label>
+            <select class="form-control" name="lead[lead_company_name]">
               <option value='none'>none</option>
               <option value="">Company</option>
             </select>
       </div><!-- form-group -->
 
       <div class="form-group">
-        <label for="project_id">Project:</label>
-            <select class="form-control" name="project_id">
-              <option value='none'>none</option>
-              <option value="">Project Title</option>
-            </select>
-      </div><!-- form-group -->
-
-      <div class="form-group">
-        <label for="lead_source">Lead Owner:</label>
-          <select class="form-control" name="user_id">
+        <label for="lead[user_id]">Lead Owner:</label>
+          <select class="form-control" name="lead[user_id]">
             <option value="" ></option>
           </select>
       </div><!-- form-group -->
