abaf7a7ec4eed7617ff4fccdebe98abd686fa8b9
Virgil Anderson <Virgil.Anderson@gmail.com>
2019-03-06 19:36:09 -0700

    Feat: Updated input names

--- public/companies/form_fields.php
+++ public/companies/form_fields.php
@@ -1,48 +1,57 @@
 <div class="form-group">
-  <label class="form-control-label" for="company_name">Company Name</label>
-  <input class="form-control" type="text" name="company_name" >
+  <label class="form-control-label" for="company[company_name]">Company Name</label>
+  <input class="form-control" type="text" name="company[company_name]" >
 </div><!-- form-group -->
 
 <div class="form-group">
-  <label class="form-control-label" for="company_address">Company Address</label>
-  <input class="form-control" type="text" name="company_address" >
+  <label class="form-control-label" for="company[company_addres]">Company Address</label>
+  <input class="form-control" type="text" name="company[company_address]" >
 </div><!-- form-group -->
 
 <div class="form-group">
-  <label class="form-control-label" for="company_city">Company City</label>
-  <input class="form-control" type="text" name="company_city" >
+  <label class="form-control-label" for="company[company_city]">Company City</label>
+  <input class="form-control" type="text" name="company[company_city]" >
 </div><!-- form-group -->
 
 <div class="form-group">
-  <label class="form-control-label" for="company_state">Company State</label>
-  <input class="form-control" type="text" name="company_state">
+  <label class="form-control-label" for="company[company_state]">Company State</label>
+  <input class="form-control" type="text" name="company[company_state]">
 </div><!-- form-group -->
 
 <div class="form-group">
-  <label class="form-control-label" for="company_zip">Company Zip</label>
-  <input class="form-control" type="text" name="company_zip">
+  <label class="form-control-label" for="company[company_zip_code]">Company Zip</label>
+  <input class="form-control" type="text" name="company[company_zip_code]">
 </div><!-- form-group -->
 
 <div class="form-group">
-  <label class="form-control-label" for="company_url">Company url</label>
-  <input class="form-control" type="text" name="company_url">
+  <label class="form-control-label" for="company[company_phone]">Company phone</label>
+  <input class="form-control" type="text" name="company[company_phone]">
 </div><!-- form-group -->
 
 <div class="form-group">
-  <label class="form-control-label" for="company_phone">Phone</label>
-  <input class="form-control" type="text" name="company_phone">
+  <label class="form-control-label" for="company[company_fax]">Company fax</label>
+  <input class="form-control" type="text" name="company[company_fax]">
 </div><!-- form-group -->
 
 <div class="form-group">
-  <label class="form-control-label" for="industry">Industry</label>
-  <input class="form-control" type="text" name="industry">
+  <label class="form-control-label" for="company[company_url]">Company url</label>
+  <input class="form-control" type="text" name="company[company_url]">
 </div><!-- form-group -->
 
 <div class="form-group">
-  <label for="lead_source">Lead Owner:</label>
-    <select class="form-control" name="user_id">
-      <?php while($admin = mysqli_fetch_assoc($admin_set)){ ?>
-      <option value="<?php echo h($admin['id']); ?>"><?php echo h($admin['username']); ?></option>
+  <label for="">Industry:</label>
+    <select class="form-control" name="">
+      <?php foreach(COMPANY::INDUSTRY_OPTIONS as $industry) { ?>
+      <option value=""></option>
+      <?php } ?>
+    </select>
+</div><!-- form-group -->
+
+<div class="form-group">
+  <label for="lead_source">Company Owner:</label>
+    <select class="form-control" name="company[company_]">
+      <?php foreach($user as $user) { ?>
+        <option value='<?php echo $user->id; ?>' <?php if($user->user_username == $session->username) {echo 'selected'; } ?>><?php echo $user->user_username; ?></option>
       <?php } ?>
     </select>
 </div><!-- form-group -->
