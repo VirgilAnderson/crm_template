8ef9f4230286f1680a8c5e5506fd00f4a7768ce6
Virgil Anderson <Virgil.Anderson@gmail.com>
2019-03-10 13:03:46 -0600

    Feat: Create Task function added

--- public/tasks/form_fields.php
+++ public/tasks/form_fields.php
@@ -1,68 +1,77 @@
 <div class="form-group">
-  <label class="form-control-label" for="task_name">Task Name</label>
-  <input class="form-control" type="text" name="task_name" >
+  <label class="form-control-label" for="task[task_name]">Task Name</label>
+  <input class="form-control" type="text" name="task[task_name]" >
 </div><!-- form-group -->
 
 <div class="form-group">
-  <label for="task_type">Task Type</label>
-      <select class="form-control" name="task_type">
-        <option>Call</option>
-        <option>Email</option>
-        <option>Follow Up</option>
-        <option>Meeting</option>
-        <option>Get Started</option>
-        <option>To Do</option>
+  <label for="task[task_type]">Task Type</label>
+      <select class="form-control" name="task[task_type]">
+        <?php foreach(TASK::TASK_TYPE as $task_type_option){ ?>
+          <option value='<?php echo $task_type_option; ?>'><?php echo $task_type_option; ?></option>
+        <?php } ?>
     </select>
 </div><!-- form-group -->
 
 <div class="form-group">
-  <label for="task_state">Task State</label>
-      <select class="form-control" name="task_state">
-        <option>Open</option>
-        <option>Closed</option>
-        <option>In Progress</option>
-        <option>Waiting On Someone Else</option>
-        <option>Not Started</option>
+  <label for="task[task_status]">Task State</label>
+      <select class="form-control" name="task[task_status]">
+        <?php foreach(TASK::TASK_STATUS as $status) { ?>
+        <option value='<?php echo $status; ?>'><?php echo $status; ?></option>
+        <?php } ?>
     </select>
 </div><!-- form-group -->
 
 <div class="form-group">
-  <label for="task_description">Task Description:</label>
-  <textarea class="form-control" rows="5" name="task_description"></textarea>
+  <label for="task[task_description]">Task Description:</label>
+  <textarea class="form-control" rows="5" name="task[task_description]"></textarea>
 </div><!-- form-group -->
 
 <div class="form-group">
-  <label class="form-control-label" for="due_date">Due Date</label>
-  <input class="form-control" type="datetime-local" name="due_date">
+  <label class="form-control-label" for="task[task_due_date]">Due Date</label>
+  <input class="form-control" type="datetime-local" name="task[task_due_date]">
 </div><!-- form-group -->
 
 <div class="form-group">
-  <label for="task_source">Company:</label>
-    <select class="form-control" name="company_id">
-        <option value='none'>none</option>
-        <option value="">Company Name</option>
+  <label for="task[company_id]">Company:</label>
+    <select class="form-control" name="task[company_id]">
+      <?php foreach($company as $company) { ?>
+        <option value='<?php echo $company->id; ?>'><?php echo $company->company_name; ?></option>
+      <?php } ?>
     </select>
 </div><!-- form-group -->
 
 <div class="form-group">
-  <label for="project_id">Project:</label>
-    <select class="form-control" name="project_id">
-        <option value='none'>none</option>
-        <option value="">Project Title</option>
+  <label for="task[project_id]">Project:</label>
+    <select class="form-control" name="task[project_id]">
+        <?php foreach($project as $project) {?>
+          <option value='<?php echo $project->id; ?>'><?php echo $project->project_name; ?></option>
+        <?php  } ?>
     </select>
 </div><!-- form-group -->
 
 <div class="form-group">
-  <label for="task_source">Employee:</label>
-    <select class="form-control" name="individual_id">
-        <option value='none'>none</option>
-        <option value="">First_Name Last_Name</option>
+  <label for="task[contact_id]">Contact</label>
+    <select class="form-control" name="task[contact_id]">
+      <?php foreach($contact as $contact) { ?>
+        <option value="<?php echo $contact->id; ?>" ><?php echo $contact->full_name(); ?></option>
+      <?php } ?>
     </select>
 </div><!-- form-group -->
 
 <div class="form-group">
-  <label for="task_source">Task Owner:</label>
-    <select class="form-control" name="user_id">
-      <option value="" >Username</option>
+  <label for="task[lead_id]">Lead</label>
+    <select class="form-control" name="task[lead_id]">
+      <?php foreach($lead as $lead) { ?>
+        <option value="<?php echo $lead->id; ?>" ><?php echo $lead->full_name(); ?></option>
+      <?php } ?>
+    </select>
+</div><!-- form-group -->
+
+<div class="form-group">
+  <label for="task[user_id]">Task Owner</label>
+    <select class="form-control" name="task[user_id]">
+      <?php foreach($user as $user ) {?>
+        <option value="<?php echo $user->id; ?>" ><?php echo $user->user_username; ?></option>
+      <?php } ?>
     </select>
 </div><!-- form-group -->
--- public/tasks/new.php
+++ public/tasks/new.php
@@ -3,6 +3,53 @@
   // Ensure User Logged In
   require_login();
 
+  // Find all user query
+  $sql = "SELECT * FROM user ";
+  $sql .= "ORDER BY id DESC";
+  $user = User::find_by_sql($sql);
+
+  // Find all companies query
+  $sql = "SELECT * FROM company ";
+  $sql .= "ORDER BY id DESC";
+  $company = Company::find_by_sql($sql);
+
+  // Find all contacts query
+  $sql = "SELECT * FROM contact ";
+  $sql .= "ORDER BY id DESC";
+  $contact = Contact::find_by_sql($sql);
+
+  // Find all leads query
+  $sql = "SELECT * FROM lead ";
+  $sql .= "ORDER BY id DESC";
+  $lead = Lead::find_by_sql($sql);
+
+  // Find all projects query
+  $sql = "SELECT * FROM project ";
+  $sql .= "ORDER BY id DESC";
+  $project = Project::find_by_sql($sql);
+
+  // If Post Request Process form
+  if(is_post_request()) {
+    // Get input data
+    $args = $_POST['task'];
+    $task = new Task($args);
+    $result = $task->save();
+
+    // Verify success
+    if($result === true){
+      $new_id = $task->id;
+      // Add session message
+      $session->message('Task was created successfully');
+      redirect_to(url_for('tasks/detail.php?task_id=' . $new_id));
+    } else {
+      echo 'error';
+    }
+
+  } else {
+    // Display blank Form
+    $task = new Task;
+  }
+
 ?>
 
 <?php $page_title = "New task"; ?>
@@ -19,7 +66,7 @@
 
 <!-- main container -->
 <div class='container'>
-  <form class="col-sm-6" action=""  method="post">
+  <form class="col-sm-6" action="<?php echo url_for('tasks/new.php'); ?>"  method="post">
       <h2><i class="far fa-plus-square"></i> New Task</h2>
       <fieldset class="form-group">
         <legend>Fill in the form to create a new task</legend>
