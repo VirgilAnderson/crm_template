3d50d5aacb914554d7b7384fd75ba2da154df3b7
Virgil Anderson <Virgil.Anderson@gmail.com>
2019-03-20 20:44:16 -0600

    Feat: Added active function to task pagination controls

--- public/javascript/task_control.js
+++ public/javascript/task_control.js
@@ -6,6 +6,11 @@
     // Add active to the selected class
     var pane = document.getElementById(selected);
     pane.classList.add("active");
+
+    // Add active to the active pagination button
+    var link_id = "link_" + selected;
+    var link = document.getElementById(link_id);
+    link.classList.add("active");
   }
   initializeTask();
 
@@ -19,12 +24,27 @@
     // Add active to the selected class
     var pane = document.getElementById(selected);
     pane.classList.add("active");
+
+    // Add active to the active pagination button
+    var link_id = "link_" + selected;
+    var link = document.getElementById(link_id);
+    link.classList.add("active");
   }
 
   function clear(){
-    // get all elements
+    // get all task elements
     var element = document.getElementsByClassName('task active');
+
+    // Clear elements
     for(var i=0; i <= element.length; i++){
         element[i].className = 'task';
+    }
+
+    // Get all active task links
+    var link_element = document.getElementsByClassName('page-item task_link active');
+
+    // Clear elements
+    for(var i=0; i <= link_element.length; i++) {
+      link_element[i].className = 'page-item task_link';
     }
   }
