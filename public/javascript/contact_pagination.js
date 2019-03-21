8859dd635bfcd5674afbf5cb09f719f2ec89898d
Virgil Anderson <Virgil.Anderson@gmail.com>
2019-03-21 11:59:35 -0600

    Feat: Adding pages to support ajax pane pagination

---
+++ public/javascript/contact_pagination.js
@@ -0,0 +1,22 @@
+// Create DOM variables
+var contact_container = document.getElementById('contact_page');
+
+
+// Spinner Functions
+function showSpinner() {
+  var spinner = document.getElementById("spinner");
+  spinner.style.display = 'block';
+}
+
+function hideSpinner() {
+  var spinner = document.getElementById("spinner");
+  spinner.style.display = 'none';
+}
+
+// Turn returned data to div
+
+// Ajax Call
+
+// Event Listeners
+
+// Load first page with Ajax
