612e3989df96223127e93b708073b0c3aff2c1c0
Virgil Anderson <Virgil.Anderson@gmail.com>
2019-03-21 14:01:12 -0600

    Feat: Added ajax call to pull up first result.

--- public/javascript/contact_pagination.js
+++ public/javascript/contact_pagination.js
@@ -1,5 +1,5 @@
 // Create DOM variables
-var contact_container = document.getElementById('contact_page');
+var contact_container = document.getElementById('contact_pane');
 
 
 // Spinner Functions
@@ -14,9 +14,48 @@ function hideSpinner() {
 }
 
 // Turn returned data to div
+function appendToDiv(div, new_html) {
+  // Put the new HTML into a temp div
+  // This causes browser to parse it as elements.
+  var temp = document.createElement('div');
+  temp.innerHTML = new_html;
+
+  // Then we can find and work with those elements.
+  // Use firstElementChild b/c of how DOM treats whitespace.
+  var class_name = temp.firstElementChild.className;
+  var items = temp.getElementsByClassName(class_name);
+
+  var len = items.length;
+  for(i=0; i < len; i++) {
+    div.appendChild(items[0]);
+  }
+}
+
 
 // Ajax Call
+function build_contact_pane() {
+
+  showSpinner();
+
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', '../../private/shared/ajax/contact_pagination.php?contact_pane=1&uid=2', true);
+  xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+  xhr.onreadystatechange = function () {
+    if(xhr.readyState == 4 && xhr.status == 200) {
+      var result = xhr.responseText;
+      console.log('Result: ' + result);
+
+      hideSpinner();
+      // append results to end of blog posts
+      appendToDiv(contact_container, result);
+
+    }
+  };
+  xhr.send();
+}
 
 // Event Listeners
 
 // Load first page with Ajax
+build_contact_pane();
